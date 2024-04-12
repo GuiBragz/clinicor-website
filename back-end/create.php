2640b96aeb6c41350bb95679d041f0c2cc6c136d
João Victor Amaral <152905979+JVAmaralR@users.noreply.github.com>
2024-04-12 11:31:42 -0300

    Inicio do Back-end e conexão do front com banco de dados
    Iniciamos a criação do CRUD, fizemos o create e fizemos a conexão do registro com o banco de dados e corrigimos um erro no codigo do login que estava aparecendo um titulo em preto html no meio da tela.
    
    Co-Authored-By: EliasJR <163365225+DevzEliasJR@users.noreply.github.com>
    Co-Authored-By: Irlan Ferreira <97058040+irlanferreira@users.noreply.github.com>
    Co-Authored-By: Xande26 <163211339+Xande26@users.noreply.github.com>

---
+++ back-end/create.php
@@ -0,0 +1,55 @@
+<?php
+    // Importar o arquivo da conexão do banco de dados.
+    include_once 'connection.php';
+
+    // Função que verifica se já existe um usuário com o mesmo cpf ou email.
+    function usuarioJaExiste($conexao, $cpf, $email) {
+        $count = 0;
+
+        $stmt = $conexao->prepare("SELECT COUNT(*) FROM usuarios WHERE cpf = ? OR email = ?");
+        $stmt->bind_param("ss", $cpf, $email);
+        $stmt->execute();
+        $stmt->bind_result($count);
+        $stmt->fetch();
+        $stmt->close();
+        
+        return $count > 0;
+    }
+
+    // Funcção para cadastrar um novo usuário.
+    function cadastrarUsuarioPaciente($nomeCompleto, $cpf, $email, $senha) {
+        $conexao = getConnection();
+        if (usuarioJaExiste($conexao, $cpf, $email)) {
+            echo "<script>window.alert('Erro ao cadastrar usuário! Email ou CPF já existem.');</script>";
+            echo "<script>window.location.href = '../cadastro.html';</script>";
+            exit;
+        }
+
+        // Funcção para inserir dados na tabela usuários.
+        $stmt = $conexao->prepare("INSERT INTO usuarios (nome, cpf, email, senha, tipo, DataRegistro) VALUES (?, ?, ?, ?, 'paciente', NOW())");
+        // Preenche os espaços em branco (as interrogações) com os valores reais.
+        $stmt->bind_param("ssss", $nomeCompleto, $cpf, $email, $senha);
+
+        // Executar a função SQL.
+        if ($stmt->execute()) {
+            echo "<script>window.alert('Cadastro realizado com sucesso!');</script>";
+            echo "<script>window.location.href = '../login.html';</script>";
+        } else {
+            echo "Ocorreu um erro. Tente novamente em alguns instantes.";
+        }
+
+        $stmt->close();
+        $conexao->close();
+    }
+
+    // Verifica se o método de requisição HTTP é do tipo "POST".
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Obtém os dados do formulário.
+        $nomeCompleto = $_POST["nome-completo"];
+        $cpf = $_POST["cpf"];
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+        // Executa a função.
+        cadastrarUsuarioPaciente($nomeCompleto, $cpf, $email, $senha);
+    }
+?>
